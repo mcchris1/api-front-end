39df26a0e2fdde4805bb8442cd537e7115d10ae6
michae.lchristenson726@yahoo.com <121463561+mcchris1@users.noreply.github.com>
2023-04-24 15:14:06 -0400

    init

---
+++ api-front-end/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { useState, useEffect } from 'react';
+
+function App() {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    fetch(``)
+  })
+  return <h1>Data</h1>; 
+}
+
+export default App;
