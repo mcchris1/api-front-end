d542ec991df6ec5b3ee25005c91d94b5a6343012
michae.lchristenson726@yahoo.com <121463561+mcchris1@users.noreply.github.com>
2023-04-24 15:23:42 -0400

    fetches data with Hooks

--- api-front-end/src/App.js
+++ api-front-end/src/App.js
@@ -4,8 +4,16 @@ import { useState, useEffect } from 'react';
 function App() {
   const [data, setData] = useState(null);
   useEffect(() => {
-    fetch(``)
-  })
+    fetch(
+      `https://simple-pokemon-api.herokuapp.com`
+      )
+      .then((response) => response.json())
+      .then(setData);
+  }, []);
+  if(data) 
+    return (
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    );
   return <h1>Data</h1>; 
 }
 
