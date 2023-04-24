e13ce7e6cdc64469051f232613aa8d2580900dcb
michae.lchristenson726@yahoo.com <121463561+mcchris1@users.noreply.github.com>
2023-04-24 16:11:39 -0400

    handles load states

--- api-front-end/src/App.js
+++ api-front-end/src/App.js
@@ -7,27 +7,35 @@ function Chinpokomon({ name, weight }) {
      <h1>{name}</h1>
      <p>{weight}</p>
      <img src="https://picsum.photos/200" alt="placeholder image for "{name} />
-
-   </div>
-  )
+    </div>
+  );
 }
 
 function App() {
   const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  
   useEffect(() => {
+    setLoading(true);
     fetch(
       `https://simple-pokemon-api.herokuapp.com`
       )
       .then((response) => response.json())
-      .then(setData);
+      .then(setData)
+      .then(() => setLoading(false))
+      .catch(setError);
   }, []);
-  if(data) 
-    return (
-      <Chinpokomon 
-        name={data.name}
-        weight={data.weight}
-      />
-  return <h1>Data</h1>; 
+
+  if(loading) return <h1>Loading...</h1>;
+  if(error) return <pre>{JSON.stringify(error)}</pre>;
+  if(!data) return null;
+  return (
+    <Chinpokomon 
+      name={data.name}
+      weight={data.weight}
+    />
+  );
 }
 
 export default App;
