e24f75faca4a9c71dead7791b35c5ffd9b09949d
michae.lchristenson726@yahoo.com <121463561+mcchris1@users.noreply.github.com>
2023-04-24 15:50:56 -0400

    displays data from API

--- api-front-end/src/App.js
+++ api-front-end/src/App.js
@@ -1,6 +1,17 @@
 import './App.css';
 import { useState, useEffect } from 'react';
 
+function Chinpokomon({ name, weight }) {
+  return (
+    <div>
+     <h1>{name}</h1>
+     <p>{weight}</p>
+     <img src="https://picsum.photos/200" alt="placeholder image for "{name} />
+
+   </div>
+  )
+}
+
 function App() {
   const [data, setData] = useState(null);
   useEffect(() => {
@@ -12,8 +23,10 @@ function App() {
   }, []);
   if(data) 
     return (
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-    );
+      <Chinpokomon 
+        name={data.name}
+        weight={data.weight}
+      />
   return <h1>Data</h1>; 
 }
 
