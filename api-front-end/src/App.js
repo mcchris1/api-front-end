2e18af21d44d824756596f83c98d0309a9926014
michae.lchristenson726@yahoo.com <121463561+mcchris1@users.noreply.github.com>
2023-05-02 13:58:11 -0400

    what is this mess

--- api-front-end/src/App.js
+++ api-front-end/src/App.js
@@ -1,40 +1,56 @@
-import './App.css';
 import { useState, useEffect } from 'react';
 
-function Chinpokomon({ name, weight }) {
-  return (
-    <div>
-     <h1>{name}</h1>
-     <p>{weight}</p>
-     <img src="https://picsum.photos/200" alt="placeholder image for "{name} />
-    </div>
-  );
-}
+// function Chinpokomon({ name, weight }) {
+//   return (
+//     <div>
+//      <h1>{name}</h1>
+//      <p>{weight}</p>
+//      <img src="https://picsum.photos/200" alt="placeholder image for "{name} />
+//     </div>
+//   );
+// }
 
 function App() {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState([]);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
+
+  let [number, setNumber] = useState(0)
+
+  const destroy = () => {
+    setNumber(number++)
+  }
   
   useEffect(() => {
     setLoading(true);
     fetch(
-      `https://simple-pokemon-api.herokuapp.com`
+      `https://simple-pokemon-api.herokuapp.com/pokemon`
       )
-      .then((response) => response.json())
-      .then(setData)
+      .then(response => response.json())
+      .then(pokemon => setData(pokemon))
       .then(() => setLoading(false))
       .catch(setError);
   }, []);
+
+  console.log(data)
 
   if(loading) return <h1>Loading...</h1>;
   if(error) return <pre>{JSON.stringify(error)}</pre>;
   if(!data) return null;
   return (
-    <Chinpokomon 
-      name={data.name}
-      weight={data.weight}
-    />
+    // <Chinpokomon 
+    //   name={data.name}
+    //   weight={data.weight}
+    // />
+    <>
+    {/* {data.map(pokemon => 
+    
+    <h2>{pokemon.name}</h2>
+
+    )} */}
+    <h1>{number}</h1>
+    <button onClick={destroy}>+1</button>
+    </>
   );
 }
 
